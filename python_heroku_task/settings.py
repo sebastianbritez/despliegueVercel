2298541d5a203fd7adacc7dec87a4e35aa0e17b1
sebastianbritez <sebastianbritezdevelop@gmail.com>
2024-04-10 11:41:06 -0400

    Update settings.py

--- python_heroku_task/settings.py
+++ python_heroku_task/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-tlv)36c^h%me#0j)!67193036i0(q!%)rm_9k19_&32jf9!t3b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*", ".vercel.app"]
+ALLOWED_HOSTS = ['*', '.vercel.app', '.now.sh']
 
 
 # Application definition
